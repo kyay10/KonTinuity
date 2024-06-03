6e8e599aace4ef1b51ad47dff6a00b6aa5032924
Youssef Shoaib <canonballt@gmail.com>
2024-06-03 16:26:14 +0100

    Move example6FromDBDCPaper test to ReaderTest and add more checks

--- library/src/commonTest/kotlin/ReaderTest.kt
+++ library/src/commonTest/kotlin/ReaderTest.kt
@@ -3,14 +3,40 @@ import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
 class ReaderTest {
-    @Test
-    fun readerMonad() = runTest {
-      runReader(1) reader@{
-        newReset reset@{
-          pushReader(2) {
-            this@reset.shift { f -> this@reader.get() }
-          }
+  @Test
+  fun simple() = runTest {
+    runReader(1) reader@{
+      newReset reset@{
+        pushReader(2) {
+          this@reset.shift { f -> this@reader.get() }
         }
-      } shouldBe 1
-    }
+      }
+    } shouldBe 1
+  }
+
+  // https://www.brinckerhoff.org/clements/csc530-sp08/Readings/kiselyov-2006.pdf
+  @Test
+  @Suppress("UNCHECKED_CAST")
+  fun example6FromDBDCPaper() = runTest {
+    runCC {
+      val p = Reader<Int>()
+      val r = Reader<Int>()
+      val tag = Prompt<Any?>()
+      val f =
+        tag.pushPrompt(p.context(1)) {
+          r.pushReader(10) {
+            tag.shift<Int, _> {
+              p.get() shouldBe 1
+              it
+            } shouldBe 0
+            p.get() + r.get()
+          }
+      } as suspend (Int) -> Int
+      p.pushReader(2) {
+        r.pushReader(20) {
+          f(0)
+        }
+      }
+    } shouldBe 12
+  }
 }
--- library/src/commonTest/kotlin/StateTest.kt
+++ library/src/commonTest/kotlin/StateTest.kt
@@ -26,29 +26,4 @@ class StateTest {
     }
     result shouldBe CounterState(4)
   }
-
-  // https://www.brinckerhoff.org/clements/csc530-sp08/Readings/kiselyov-2006.pdf
-  @Test
-  @Suppress("UNCHECKED_CAST")
-  fun example6FromDBDCPaper() = runTest {
-    runCC {
-      val p = Reader<Int>()
-      val r = Reader<Int>()
-      val f = p.pushReader<_, Any?>(1) {
-        newReset {
-          r.pushReader(10) {
-            shift<Int, _> {
-              it
-            } shouldBe 0
-            p.get() + r.get()
-          }
-        }
-      } as suspend (Int) -> Int
-      p.pushReader(2) {
-        r.pushReader(20) {
-          f(0)
-        }
-      }
-    } shouldBe 12
-  }
 }
