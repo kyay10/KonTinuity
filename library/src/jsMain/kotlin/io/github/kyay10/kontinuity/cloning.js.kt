66024da0c98a27c2c52bec122acd8ba7c6a57b91
Youssef Shoaib <canonballt@gmail.com>
2026-03-15 18:49:23 +0000

    Speedup JS cloning Upgrade Kotlin

--- library/src/jsMain/kotlin/io/github/kyay10/kontinuity/cloning.js.kt
+++ library/src/jsMain/kotlin/io/github/kyay10/kontinuity/cloning.js.kt
@@ -2,10 +2,19 @@ package io.github.kyay10.kontinuity
 
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.intrinsics.*
 
 internal actual const val SUPPORTS_MULTISHOT = true
 
-internal actual val Continuation<*>.completion: Continuation<*>? get() = this.asDynamic().resultContinuation_1
+private val resultContinuationName: String = run {
+  lateinit var cont: Continuation<*>
+  val completion = Continuation<Unit>(EmptyCoroutineContext) {}
+  suspend { suspendCoroutineUninterceptedOrReturn<Unit> { cont = it } }.asDynamic()(completion)
+  js("Object.keys(cont).find((name) => cont[name] === completion)")
+}
+
+internal actual val Continuation<*>.completion: Continuation<*>? get() = asDynamic()[resultContinuationName]
 
 internal actual fun <T> Continuation<T>.invokeCopied(
   completion: Continuation<*>,
@@ -15,7 +24,7 @@ internal actual fun <T> Continuation<T>.invokeCopied(
   val cont = this
   val copy: Continuation<T> = js("Object.create(Object.getPrototypeOf(cont))")
   // add all properties, which includes resultContinuation, intercepted, and _context
-  js("Object.defineProperties(copy, Object.getOwnPropertyDescriptors(cont))")
+  js("Object.assign(copy, cont)")
   // call constructor, which resets the coroutine internal state
   copy.initialize(completion)
   // thus, we copy the internal state (advancing the coroutine)
